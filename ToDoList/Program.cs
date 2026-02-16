7fbb4988ca6530865a114b86661498969b9755a0
Netherese <LellianaLil@outlook.com>
2026-02-16 17:57:02 +0100

    Zmena kodu pro ozkouseni ruznych principu

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 Console.WriteLine("Hello, Lada!");
@@ -7,25 +8,46 @@ Console.WriteLine("[A]dd todo");
 Console.WriteLine("[R]emove todo");
 Console.WriteLine("[E]xit");
 
-string userInput = Console.ReadLine();
-Console.WriteLine($"You entered: {userInput}");
+var userChoice = Console.ReadLine();
+bool isLong = IsLong(userChoice);
 
-bool isUserInputABC = userInput == "ABC";
-Console.WriteLine(isUserInputABC ? "User input is ABC" : "User input is not ABC");
+bool IsLong(string input)
+{
+    return input.Length > 10;
+}
 
-var number = 0;
-string result;
-if (number < 0)
-{
-    result = "negative";
-            }
-else if (number == 0)
-{
-    result = "zero";
-            }
-else
-{
-    result = "positive";
-            }
+var result = Add(10, 5);
+Console.WriteLine("The result of adding 10 and 5 is: {result}");
 
-Console.ReadKey();
+//if (userChoice == "S")
+//{
+//    PrintSelectedOption("Showing all todos...");
+//}
+//else if (userChoice == "A")
+//{
+//    PrintSelectedOption("Adding a new todo...");
+//}
+//else if (userChoice == "R")
+//{
+//    PrintSelectedOption("Removing a todo...");
+//}
+//else if (userChoice == "E")
+//{
+//    PrintSelectedOption("Exiting the application...");
+//}
+//else
+//{
+//    PrintSelectedOption("Invalid choice. Please try again.");
+//}
+
+//Console.ReadKey();
+//void PrintSelectedOption(string selectedOption)
+//{
+//    Console.WriteLine($"You selected: {selectedOption}");
+//}
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
